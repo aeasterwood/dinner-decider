c6776167daa3f32405d74b7cf7e977465677e7a4
A Easterwood <aliciaeasterwood@gmail.com>
2019-08-19 23:21:12 -0500

    begin configure webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
 	constructor (props) {
 		super(props);
@@ -103,7 +106,7 @@ const Choices = (props) => {
 	return (
 		<div>
 			<button onClick={props.handleDeleteAll}>Delete All</button>
-			{props.choices.length === 0 && <p>Add some choices to get started</p>}
+			{props.choices.length === 0 && <p>To get started, add some choices below.</p>}
 			{props.choices.map((choice) => (
 				<Choice key={choice} choiceText={choice} handleDeleteChoice={props.handleDeleteChoice} />
 			))}
